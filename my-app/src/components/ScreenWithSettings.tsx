e8f6982f92fd47288d50295bff6262eb14f116fd
YarGromov <1l1l1arr1l1l1@gmail.com>
2023-02-28 17:39:16 +0100

    onChangeHandler

--- my-app/src/components/ScreenWithSettings.tsx
+++ my-app/src/components/ScreenWithSettings.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {ChangeEvent, ChangeEventHandler} from 'react';
 import s from './ScreenWithSettings.module.css'
 
 
 export const ScreenWithSettings = () => {
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        const result = e.currentTarget.value;
+        console.log(result)
+    }
     return (
         <div className={s.ScreenWithSettings}>
             <div className={s.firstContainer}>
-                <div>max value: <input type="number"/></div>
-                <div>start value: <input type="number"/></div>
+                <div>max value: <input onChange={onChangeHandler} type="number"/></div>
+                <div>start value: <input onChange={onChangeHandler} type="number"/></div>
             </div>
             <div className={s.secondContainer}>
                 <button>set</button>
@@ -15,4 +20,3 @@ export const ScreenWithSettings = () => {
         </div>
     );
 };
-
